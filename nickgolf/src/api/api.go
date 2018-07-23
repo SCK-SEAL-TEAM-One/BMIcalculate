ad8b0127e13fc2c545e46d6675cd684098fd3095
sakarin.po <sakarin.po@baac.or.th>
2018-07-23 11:44:49 +0700

    [Edited]:แ้ไข function ApiHandler

--- nickgolf/src/api/api.go
+++ nickgolf/src/api/api.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"bmicalculate"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 type BmiStatus struct {
@@ -11,7 +13,15 @@ type BmiStatus struct {
 }
 
 func ApiHandler(w http.ResponseWriter, r *http.Request) {
-	bmiStatus := BmiStatus{Bmi: 27.7, Status: "อ้วน"}
+	height := r.URL.Query().Get("height")
+	weight := r.URL.Query().Get("weight")
+	heightConv, _ := strconv.ParseFloat(height, 64)
+	weightConv, _ := strconv.ParseFloat(weight, 64)
+
+	bmiCalculate := bmicalculate.BmiCal(heightConv, weightConv)
+	bmiStatusCheck := bmicalculate.BmiStatusCheck(bmiCalculate)
+
+	bmiStatus := BmiStatus{Bmi: bmiCalculate, Status: bmiStatusCheck}
 	bmiJson, _ := json.Marshal(bmiStatus)
 	w.Write(bmiJson)
 }
